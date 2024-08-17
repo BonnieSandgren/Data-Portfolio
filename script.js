98f46a23a2c1e11812c483ecfef285d4598e6cc0
Bonnie Sandgren <bonniesandgren@gmail.com>
2024-08-17 17:40:09 -0600

    new html, css and js for portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic form validation (can be expanded based on your needs)
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    alert('Thank you for reaching out! I will get back to you soon.');
+    event.preventDefault();
+});
